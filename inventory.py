8038422269a7e2d15059d565f28ada2e9e803396
Shobhit Sachan <31156049+sachans@users.noreply.github.com>
2018-03-05 23:15:31 +0530

    Delete inventory.py

--- inventory.py
+++
@@ -1,7 +0,0 @@
-class Item:
-    def __init__(self, name, type, description, prop):
-        self.name = name
-        self.type = type
-        self.description = description
-        self.prop = prop
-
